76347122db90b64d89ea4c15379be94a2d5f1e4e
crazyyy <crazyyy@gmail.com>
2017-03-17 16:18:53 +0200

    init

---
+++ html/blog-inner_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"8850 http:\/\/www.montway.com\/blog\/?p=8850","comments":0}]});
+}
