2d023b934a5979c51c07dfeb0f8a0bdc8f24c8d4
crazyyy <crazyyy@gmail.com>
2017-03-20 09:29:03 +0200

    before wordpressed

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_NAME', 'DBNAME');
-define('DB_USER', 'DBUSER');
-define('DB_PASSWORD', 'DBPASS');
+define('DB_NAME', 'shipcar24');
+define('DB_USER', 'shipcar24');
+define('DB_PASSWORD', 'shipcar24');
 
 define('DB_HOST', 'localhost');
 
