9465bb460caa9df4cccc4883ce14b5475d8205ac
crazyyy <crazyyy@gmail.com>
2017-04-02 23:08:57 +0300

    close project

--- wordpress/wp-content/themes/wp-shipcar/footer.php
+++ wordpress/wp-content/themes/wp-shipcar/footer.php
@@ -1,64 +1,64 @@
   <section class="call-to-action primary">
     <div class="container">
       <h3>Ready to get the best in auto transport?</h3>
-      <a href="#/get-a-quote" class="btn btn-lg btn-outline quote-link">Get An Instant Quote<i class="fa fa-chevron-right fa-fw"></i></a>
-      <p class="lead text-muted">Or call <a href="tel:8886668929">(888) 666-8929</a> to speak directly to our Car Shipping Advisors now!</p>
+      <a href="http://shipcar24.com/get-a-quote" class="btn btn-lg btn-outline quote-link">Get An Instant Quote<i class="fa fa-chevron-right fa-fw"></i></a>
+      <p class="lead text-muted">Or call <a href="tel:9163005174">(916) 300-5174</a> to speak directly to our Car Shipping Advisors now!</p>
     </div>
   </section>
   <nav class="bottom-nav">
     <div class="container">
       <div class="row">
         <div class="col-md-15 col-sm-6">
-          <h4>Why Montway</h4>
+          <h4>Why ShipCar24</h4>
           <ul>
-            <li><a href="#/reviews">Reviews</a></li>
-            <li><a href="#/who-uses-montway">Who uses Montway</a></li>
-            <li><a href="#/enterprise">Enterprise Solutions</a></li>
-            <li><a href="#/how-it-works">How it works</a></li>
+            <li><a href="#">Reviews</a></li>
+            <li><a href="#">Who uses ShipCar24</a></li>
+            <li><a href="#">Enterprise Solutions</a></li>
+            <li><a href="#">How it works</a></li>
           </ul>
         </div>
         <div class="col-md-15 col-sm-6">
           <h4>Transport Services</h4>
           <ul>
-            <li><a href="#/get-a-quote" class="quote-link">Quote Calculator</a></li>
-            <li><a href="#/enclosed">Enclosed</a></li>
-            <li><a href="#/expedited">Expedited</a></li>
-            <li><a href="#/door-to-door">Door-to-Door</a></li>
+            <li><a href="#" class="quote-link">Quote Calculator</a></li>
+            <li><a href="#">Enclosed</a></li>
+            <li><a href="#">Expedited</a></li>
+            <li><a href="#">Door-to-Door</a></li>
           </ul>
         </div>
         <div class="clear visible-sm"></div>
         <div class="col-md-15 col-sm-6">
           <h4>About</h4>
           <ul>
-            <li><a href="#/history">History</a></li>
-            <li><a href="https://careers.montway.com/">Careers</a></li>
-            <li><a href="#/terms-and-conditions">Terms &amp; Conditions</a></li>
-            <li><a href="#/privacy-policy">Privacy Policy</a></li>
-            <li><a href="#/copyright">Copyright &amp; Trademark</a></li>
+            <li><a href="#">History</a></li>
+            <li><a href="#/">Careers</a></li>
+            <li><a href="#">Terms &amp; Conditions</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Copyright &amp; Trademark</a></li>
           </ul>
         </div>
         <div class="col-md-15 col-sm-6">
           <h4>Support</h4>
           <ul>
-            <li><a href="#/contact-us">Contact Us</a></li>
-            <li><a href="#/transport-tips/how-do-pay-for-auto-transport">Auto Transport Tips</a></li>
-            <li><a href="#/help-center/">Help Center</a></li>
-            <li><a href="#/blog/">Blog</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Auto Transport Tips</a></li>
+            <li><a href="#">Help Center</a></li>
+            <li><a href="#">Blog</a></li>
           </ul>
         </div>
         <div class="montway-address col-md-15 col-sm-6 col-md-offset-0 col-sm-offset-3">
           <div itemscope="" itemtype="http://schema.org/LocalBusiness">
-            <h4 itemprop="name">Montway Auto Transport</h4>
+            <h4 itemprop="name">ShipCar24 Auto Transport</h4>
             <div itemprop="address" itemscope="" itemtype="http://schema.org/PostalAddress">
-              <div><span itemprop="streetAddress">1400 E Touhy Ave, Suite 255</span></div>
+              <div><span itemprop="streetAddress">4366 Auburn, Blvd, Suite 102</span></div>
               <div>
-                <span itemprop="addressLocality">Des Plaines</span>,
-                <span itemprop="addressRegion">IL</span>
-                <span itemprop="postalCode">60018</span>
+                <span itemprop="addressLocality">Sacramento</span>,
+                <span itemprop="addressRegion">CA</span>
+                <span itemprop="postalCode">95841</span>
               </div>
             </div>
-            <div class="map"><a href="https://www.google.com/maps/place/Montway+Auto+Transport/@42.010275,-87.888866,15z/data=!4m2!3m1!1s0x0:0x62f8eddad1445cb6" target="_blank" rel="nofollow" itemprop="maps">MAP &gt;</a></div>
-            <div class="phone"><i class="fa fa-phone">&nbsp;</i><a href="tel:2242200221"><span itemprop="telephone">(224) 220-0221</span></a></div>
+            <div class="map"><a href="https://www.google.com/maps/place/shipcar24+Auto+Transport/" target="_blank" rel="nofollow" itemprop="maps">MAP </a></div>
+            <div class="phone"><i class="fa fa-phone">&nbsp;</i><a href="tel: 9163005174"><span itemprop="telephone">(916) 300-5174</span></a></div>
           </div>
         </div>
       </div>
@@ -69,13 +69,13 @@
       <div class="row">
         <div class="col-lg-4 col-md-5">
           <ul class="social-links">
-            <li class="facebook"><a href="https://www.facebook.com/montway" target="_blank" data-link="outbound"><i class="fa fa-facebook fa-fw"></i></a></li>
-            <li class="twitter"><a href="https://www.twitter.com/montway" target="_blank" data-link="outbound"><i class="fa fa-twitter fa-fw"></i></a></li>
-            <li class="google"><a href="https://plus.google.com/105260163890437459670/posts" target="_blank" data-link="outbound"><i class="fa fa-google-plus fa-fw"></i></a></li>
-            <li class="linkedin"><a href="https://www.linkedin.com/company/montway-auto-transport" target="_blank" data-link="outbound"><i class="fa fa-linkedin fa-fw"></i></a></li>
-            <li class="vimeo"><a href="https://vimeo.com/montwayauto" target="_blank" data-link="outbound"><i class="fa fa-vimeo fa-fw"></i></a></li>
-            <li class="youtube"><a href="https://www.youtube.com/user/montwayautotransport" target="_blank" data-link="outbound"><i class="fa fa-youtube fa-fw"></i></a></li>
-            <li class="instagram"><a href="https://www.instagram.com/montwayautotransport/" target="_blank" data-link="outbound"><i class="fa fa-instagram fa-fw"></i></a></li>
+            <li class="facebook"><a href="https://www.facebook.com/shipcar24" target="_blank" data-link="outbound"><i class="fa fa-facebook fa-fw"></i></a></li>
+            <li class="twitter"><a href="https://www.twitter.com/shipcar24" target="_blank" data-link="outbound"><i class="fa fa-twitter fa-fw"></i></a></li>
+            <li class="google"><a href="https://plus.google.com/#/posts" target="_blank" data-link="outbound"><i class="fa fa-google-plus fa-fw"></i></a></li>
+            <li class="linkedin"><a href="https://www.linkedin.com/company/shipcar24-auto-transport" target="_blank" data-link="outbound"><i class="fa fa-linkedin fa-fw"></i></a></li>
+            <li class="vimeo"><a href="https://vimeo.com/shipcar24auto" target="_blank" data-link="outbound"><i class="fa fa-vimeo fa-fw"></i></a></li>
+            <li class="youtube"><a href="https://www.youtube.com/user/shipcar24autotransport" target="_blank" data-link="outbound"><i class="fa fa-youtube fa-fw"></i></a></li>
+            <li class="instagram"><a href="https://www.instagram.com/shipcar24autotransport/" target="_blank" data-link="outbound"><i class="fa fa-instagram fa-fw"></i></a></li>
           </ul>
         </div>
         <div class="col-lg-8 col-md-7">
